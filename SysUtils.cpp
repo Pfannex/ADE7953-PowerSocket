87c2343ff291b4f5f0ee566943d7136528459af9
Dr. Boris Neubert <omega@online.de>
2017-06-16 19:09:13 +0200

    Amended SysUtils

--- SysUtils.cpp
+++ SysUtils.cpp
@@ -1,9 +1,36 @@
-
+/*
 extern "C" {
   #include "user_interface.h"
 }
 
-
 uint32_t freeHeapSize() {
   return system_get_free_heap_size();
+}
+*/
+
+#include <ESP8266WiFi.h>
+
+void reboot() {
+  // due to an issue in the silicon chip,
+  // this works only after one hardware reset
+  // has occured after flashing
+  ESP.restart();
+}
+
+long freeHeapSize() {
+  return ESP.getFreeHeap();
+}
+
+long chipID() {
+  return ESP.getChipId();
+}
+
+String macAddress() {
+  
+    uint8_t mac[6];
+    char maddr[18];
+    WiFi.macAddress(mac);
+      sprintf(maddr, "%02x:%02x:%02x:%02x:%02x:%02x", 
+                mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+    return String(maddr);
 }
--- SysUtils.h
+++ SysUtils.h
@@ -1,3 +1,6 @@
-
+#include <Arduino.h>
 
 uint32_t freeHeapSize();
+void reboot();
+long chipID();
+String macAddress();
