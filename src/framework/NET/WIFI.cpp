1e1f2edb1ceafe73910ab386d4e34d4271728885
Pf@nne <pf@nne-mail.de>
2018-02-14 18:45:26 +0100

    bugfix wifi-connect with esp-core v1.6.0

--- src/framework/NET/WIFI.cpp
+++ src/framework/NET/WIFI.cpp
@@ -18,33 +18,20 @@ bool WIFI::start() {
   String psk = ffs.cfg.readItem("wifi_password");
   String mode = ffs.cfg.readItem("wifi"); // off, dhcp, manual
 
+  IPAddress address(0,0,0,0);
+  IPAddress gateway(0,0,0,0);
+  IPAddress netmask(0,0,0,0);
+  IPAddress dns(0,0,0,0);
+
   WiFiStatus = false;
   if (mode == "off"){
     logging.info("WiFi is off");
     WiFi.disconnect(true);
   }else{   //dhcp or manual
     WiFi.mode(WIFI_STA);  //only STA without AP!
-    WiFi.begin(ssid.c_str(), psk.c_str());
-  }
-
-
-
-
-/*
-  String ssid = ffs.cfg.readItem("wifi_ssid");
-  String psk = ffs.cfg.readItem("wifi_password");
-  String mode = ffs.cfg.readItem("wifi"); // off, dhcp, manual
-  IPAddress address(0,0,0,0);
-  IPAddress gateway(0,0,0,0);
-  IPAddress netmask(0,0,0,0);
-  IPAddress dns(0,0,0,0);
-
-  if (mode == "off") {
-    logging.info("WiFi is off");
-    WiFi.disconnect(true);
-  } else {
-    WiFi.mode(WIFI_STA);
-    if(mode == "manual") {
+    if(mode == "dhcp") {
+      logging.info("WiFi DHCP configuration");
+    } else {  //switch to static mode
       address.fromString(ffs.cfg.readItem("wifi_ip"));
       gateway.fromString(ffs.cfg.readItem("wifi_gateway"));
       netmask.fromString(ffs.cfg.readItem("wifi_netmask"));
@@ -54,42 +41,19 @@ bool WIFI::start() {
       logging.debug("gateway: "+gateway.toString());
       logging.debug("netmask: "+netmask.toString());
       logging.debug("DNS server: "+dns.toString());
-    } else {
-      logging.info("WiFi DHCP configuration");
-    }
 
-    logging.info("connecting WiFi to network with SSID " + ssid);
-    WiFi.begin(ssid.c_str(), psk.c_str());
-    int i = 0;
-    // wait 30 seconds for connection
-    while (WiFi.status() != WL_CONNECTED and i <= 30) {
-      delay(1000);
-      i++;
-      if (!(i % 5)) {
-        logging.info("still trying to connect...");
-      }
-    }
-
-    if (WiFi.status() == WL_CONNECTED) {
-      logging.debug("WiFi connection established");
-      WiFiStatus = true;
-      // it is ugly and unreasonable to require a connection to
-      // set these parameters but it is the only thing that works;
-      // see https://forum.arduino.cc/index.php?topic=460595.0
       if(WiFi.config(address, gateway, netmask, dns)) {
-        logging.debug("WiFi static configuration applied");
+        logging.debug("WiFi configuration applied");
+        //logging.info("local IP address switched to: " + WiFi.localIP().toString());
       } else {
-        logging.error("could not apply WiFi static configuration");
+        logging.error("could not apply WiFi configuration");
       }
-      logging.info("local IP address: " + WiFi.localIP().toString());
-      if (on_wifiConnected != nullptr)
-        on_wifiConnected(); // callback event
-    } else {
-      logging.error("WiFi unable to connect");
-      // start AP Ã¼ber Button!!! (GodMode?)
+
+
     }
+    WiFi.begin(ssid.c_str(), psk.c_str());
   }
-*/
+
 
   return WiFiStatus;
 }
@@ -116,38 +80,7 @@ void WIFI::handle(){
 //  WiFi on connection established
 //...............................................................................
 void WIFI::on_connected(){
-  String ssid = ffs.cfg.readItem("wifi_ssid");
-  String psk = ffs.cfg.readItem("wifi_password");
-  String mode = ffs.cfg.readItem("wifi"); // off, dhcp, manual
-  IPAddress address(0,0,0,0);
-  IPAddress gateway(0,0,0,0);
-  IPAddress netmask(0,0,0,0);
-  IPAddress dns(0,0,0,0);
-
-  logging.info("DHCP get local IP address: " + WiFi.localIP().toString());
-
-  if(mode == "dhcp") {
-    logging.info("WiFi DHCP configuration");
-  } else {  //switch to static mode
-    address.fromString(ffs.cfg.readItem("wifi_ip"));
-    gateway.fromString(ffs.cfg.readItem("wifi_gateway"));
-    netmask.fromString(ffs.cfg.readItem("wifi_netmask"));
-    dns.fromString(ffs.cfg.readItem("wifi_dns"));
-    logging.info("WiFi static configuration");
-    logging.debug("IP address: "+address.toString());
-    logging.debug("gateway: "+gateway.toString());
-    logging.debug("netmask: "+netmask.toString());
-    logging.debug("DNS server: "+dns.toString());
-  }
-  // it is ugly and unreasonable to require a connection to
-  // set these parameters but it is the only thing that works;
-  // see https://forum.arduino.cc/index.php?topic=460595.0
-  if(WiFi.config(address, gateway, netmask, dns)) {
-    logging.debug("WiFi configuration applied");
-    //logging.info("local IP address switched to: " + WiFi.localIP().toString());
-  } else {
-    logging.error("could not apply WiFi configuration");
-  }
+  logging.info("get local IP address: " + WiFi.localIP().toString());
 
 
 
