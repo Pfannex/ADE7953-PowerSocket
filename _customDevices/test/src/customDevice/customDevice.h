e08b43dff5cc79ffd8abe161fc5a3198ef387d7d
Pf@nne <pf@nne-mail.de>
2018-08-28 20:36:35 +0200

    add WIN rapidloader

---
+++ _customDevices/test/src/customDevice/customDevice.h
@@ -0,0 +1,41 @@
+#include "framework/Utils/Logger.h"
+#include "framework/Core/FFS.h"
+#include "Setup.h"
+#include "framework/OmniESP/Topic.h"
+#include "framework/OmniESP/Device.h"
+
+// device-specific setup
+#include "customDeviceSetup.h"
+
+// modules required by device
+#include "modules/GPIO.h"
+
+//###############################################################################
+//  Device
+//###############################################################################
+
+#define DEVICETYPE      "SimpleSwitch"
+#define DEVICEVERSION   "v1"
+
+class customDevice : public Device {
+
+public:
+  customDevice(LOGGING &logging, TopicQueue &topicQueue, FFS &ffs);
+  void start();
+  void handle();
+  String set(Topic &topic);
+  String get(Topic &topic);
+  void on_events(Topic &topic);
+
+private:
+  GPIOinput button;
+  GPIOoutput led;
+  GPIOoutput relay;
+
+  // the central modes
+  int power = 0;
+  int configMode = 0;
+  void setConfigMode(int value);
+  void setPowerMode(int value);
+  void setLedMode(int value);
+};
--- rapidloader.bat
+++ rapidloader.bat
@@ -1,7 +1,148 @@
 @echo off
-echo
-echo
+CLS
 
-title This is your first batch script!
-echo Welcome to batch scripting!
-pause
+REM ----------------------------------------------------------------------------
+REM MENU load or archive
+REM ----------------------------------------------------------------------------
+
+:start
+ECHO.
+ECHO what to do?
+ECHO -------------------------------------------
+ECHO.
+ECHO [1] load device from archive
+ECHO [2] archive modified device
+ECHO [3] copy new clean OmniESP.json
+ECHO [4] EXIT
+SET /P toDo=
+
+IF %toDo% == 1 GOTO loadDevice
+IF %toDo% == 2 GOTO archiveDevice
+IF %toDo% == 3 GOTO copyOmniesp
+IF %toDo% == 4 GOTO end
+
+CLS
+ECHO Invalid Selection! Try again
+PAUSE
+CLS
+GOTO start
+
+
+REM ----------------------------------------------------------------------------
+:loadDevice
+CLS
+ECHO load device from framework
+ECHO -------------------------------------------
+ECHO.
+REM -----------------------------------------
+REM clean framework
+REM -----------------------------------------
+ECHO clean framework files....
+DEL data\customDevice\*.json
+DEL src\customDevice\customDevice*.*
+ECHO done
+ECHO.
+
+REM -----------------------------------------
+REM read directory and show
+REM -----------------------------------------
+
+setlocal EnableDelayedExpansion
+CD _customDevices
+SET /a i = 0
+FOR /D %%G in ("*") DO (
+  SET /a i+=1
+  SET "deviceDir[!i!]=%%~nxG"
+  ECHO [!i!] %%~nxG
+)
+CD ..
+ECHO.
+ECHO select Device.....
+
+REM -----------------------------------------
+REM choose device
+REM -----------------------------------------
+
+SET /P dirCounter=
+CLS
+ECHO load Device: !deviceDir[%dirCounter%]!
+ECHO -------------------------------------------
+ECHO.
+
+REM -----------------------------------------
+REM copy Files
+REM -----------------------------------------
+
+ECHO copy files...
+copy %~d0%~p0_customDevices\!deviceDir[%dirCounter%]!\data\customDevice\*.json data\customDevice
+copy %~d0%~p0_customDevices\!deviceDir[%dirCounter%]!\src\customDevice\customDevice*.* src\customDevice
+ECHO done
+
+GOTO end
+
+REM ----------------------------------------------------------------------------
+:archiveDevice
+CLS
+ECHO archive Device
+ECHO -------------------------------------------
+ECHO.
+
+REM -----------------------------------------
+REM read directory and show
+REM -----------------------------------------
+
+setlocal EnableDelayedExpansion
+CD _customDevices
+SET /a i = 0
+FOR /D %%G in ("*") DO (
+  SET /a i+=1
+  SET "deviceDir[!i!]=%%~nxG"
+  ECHO [!i!] %%~nxG
+)
+CD ..
+ECHO.
+ECHO select Device.....
+
+REM -----------------------------------------
+REM choose device
+REM -----------------------------------------
+
+SET /P dirCounter=
+CLS
+ECHO archive Device: !deviceDir[%dirCounter%]!
+ECHO -------------------------------------------
+ECHO.
+
+REM -----------------------------------------
+REM clean archive
+REM -----------------------------------------
+ECHO clean archive files !deviceDir[%dirCounter%]!....
+DEL _customDevices\!deviceDir[%dirCounter%]!\data\customDevice\*.json
+DEL _customDevices\!deviceDir[%dirCounter%]!\firmware\*.bin
+DEL _customDevices\!deviceDir[%dirCounter%]!\firmware\*.tar
+DEL _customDevices\!deviceDir[%dirCounter%]!\src\customDevice\customDevice*.*
+ECHO done
+ECHO.
+
+REM -----------------------------------------
+REM copy Files
+REM -----------------------------------------
+
+ECHO archive files to !deviceDir[%dirCounter%]!
+copy %~d0%~p0data\customDevice\*.json _customDevices\!deviceDir[%dirCounter%]!\data\customDevice
+copy %~d0%~p0update\*.* _customDevices\!deviceDir[%dirCounter%]!\firmware
+copy %~d0%~p0src\customDevice\customDevice*.* _customDevices\!deviceDir[%dirCounter%]!\src\customDevice
+ECHO done
+
+GOTO end
+
+REM ----------------------------------------------------------------------------
+:copyOmniesp
+CLS
+ECHO copy OmniESP to data DIR
+ECHO -------------------------------------------
+ECHO.
+ECHO copy files...
+copy %~d0%~p0_customDevices\OmniESP.json data
+
+:end
