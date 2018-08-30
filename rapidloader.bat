695f881e7c3b686716eef17c03e6d56b608d3474
Pf@nne <pf@nne-mail.de>
2018-08-30 19:59:14 +0200

    update rapidloader

--- rapidloader.bat
+++ rapidloader.bat
@@ -6,143 +6,193 @@ REM MENU load or archive
 REM ----------------------------------------------------------------------------
 
 :start
-ECHO.
-ECHO what to do?
-ECHO -------------------------------------------
-ECHO.
-ECHO [1] load device from archive
-ECHO [2] archive modified device
-ECHO [3] copy new clean OmniESP.json
-ECHO [4] EXIT
-SET /P toDo=
+  ECHO.
+  ECHO what to do?
+  ECHO -------------------------------------------
+  ECHO.
+  ECHO [1] load device from archive
+  ECHO [2] archive modified device
+  ECHO [3] copy new clean OmniESP.json
+  ECHO [4] create NEW Device
+  ECHO [5] create NEW Module
+  ECHO [6] EXIT
+  SET /P toDo=
 
-IF %toDo% == 1 GOTO loadDevice
-IF %toDo% == 2 GOTO archiveDevice
-IF %toDo% == 3 GOTO copyOmniesp
-IF %toDo% == 4 GOTO end
+  IF %toDo% == 1 GOTO loadDevice
+  IF %toDo% == 2 GOTO archiveDevice
+  IF %toDo% == 3 GOTO copyOmniesp
+  IF %toDo% == 4 GOTO newDevice
+  IF %toDo% == 5 GOTO newModule
+  IF %toDo% == 6 GOTO end
 
-CLS
-ECHO Invalid Selection! Try again
-PAUSE
-CLS
-GOTO start
-
+  CLS
+  ECHO Invalid Selection! Try again
+  PAUSE
+  CLS
+  GOTO start
 
 REM ----------------------------------------------------------------------------
+REM [1] load device from archive
+REM ----------------------------------------------------------------------------
+
 :loadDevice
-CLS
-ECHO load device from framework
-ECHO -------------------------------------------
-ECHO.
-REM -----------------------------------------
-REM clean framework
-REM -----------------------------------------
-ECHO clean framework files....
-DEL data\customDevice\*.json
-DEL src\customDevice\customDevice*.*
-ECHO done
-ECHO.
+  CLS
+  ECHO load device from framework
+  ECHO -------------------------------------------
+  ECHO.
 
-REM -----------------------------------------
-REM read directory and show
-REM -----------------------------------------
+  REM -----------------------------------------
+  REM clean framework
+  REM -----------------------------------------
+      ECHO clean framework files....
+      DEL data\customDevice\*.json
+      DEL src\customDevice\customDevice*.*
+      ECHO done
+      ECHO.
 
-setlocal EnableDelayedExpansion
-CD _customDevices
-SET /a i = 0
-FOR /D %%G in ("*") DO (
-  SET /a i+=1
-  SET "deviceDir[!i!]=%%~nxG"
-  ECHO [!i!] %%~nxG
-)
-CD ..
-ECHO.
-ECHO select Device.....
+  REM -----------------------------------------
+  REM read directory and show
+  REM -----------------------------------------
+  :readDir
+    setlocal EnableDelayedExpansion
+    CD _customDevices
+    SET /a i = 0
+    FOR /D %%G in ("*") DO (
+      SET /a i+=1
+      SET "deviceDir[!i!]=%%~nxG"
+      ECHO [!i!] %%~nxG
+    )
+    SET /a i+=1
+    ECHO [!i!] EXIT
+    CD ..
+    ECHO.
+
+    ECHO select Device.....
 
 REM -----------------------------------------
 REM choose device
 REM -----------------------------------------
 
-SET /P dirCounter=
-CLS
-ECHO load Device: !deviceDir[%dirCounter%]!
-ECHO -------------------------------------------
-ECHO.
+  SET /P dirCounter=
+  IF %dirCounter% == !i! GOTO end
 
-REM -----------------------------------------
-REM copy Files
-REM -----------------------------------------
+  CLS
+  ECHO load Device: !deviceDir[%dirCounter%]!
+  ECHO -------------------------------------------
+  ECHO.
 
-ECHO copy files...
-copy %~d0%~p0_customDevices\!deviceDir[%dirCounter%]!\data\customDevice\*.json data\customDevice
-copy %~d0%~p0_customDevices\!deviceDir[%dirCounter%]!\src\customDevice\customDevice*.* src\customDevice
-ECHO done
+  REM -----------------------------------------
+  REM copy Files
+  REM -----------------------------------------
 
-GOTO end
+  ECHO copy files...
+  copy %~d0%~p0_customDevices\!deviceDir[%dirCounter%]!\data\customDevice\*.json data\customDevice
+  copy %~d0%~p0_customDevices\!deviceDir[%dirCounter%]!\src\customDevice\customDevice*.* src\customDevice
+  ECHO done
+  ECHO.
+  GOTO end
 
 REM ----------------------------------------------------------------------------
+REM [2] archive modified device
+REM ----------------------------------------------------------------------------
+
 :archiveDevice
-CLS
-ECHO archive Device
-ECHO -------------------------------------------
-ECHO.
+  CLS
+  ECHO archive Device
+  ECHO -------------------------------------------
+  ECHO.
 
-REM -----------------------------------------
-REM read directory and show
-REM -----------------------------------------
+  REM -----------------------------------------
+  REM read directory and show
+  REM -----------------------------------------
 
-setlocal EnableDelayedExpansion
-CD _customDevices
-SET /a i = 0
-FOR /D %%G in ("*") DO (
+  setlocal EnableDelayedExpansion
+  CD _customDevices
+  SET /a i = 0
+  FOR /D %%G in ("*") DO (
+    SET /a i+=1
+    SET "deviceDir[!i!]=%%~nxG"
+    ECHO [!i!] %%~nxG
+  )
   SET /a i+=1
-  SET "deviceDir[!i!]=%%~nxG"
-  ECHO [!i!] %%~nxG
-)
-CD ..
-ECHO.
-ECHO select Device.....
+  ECHO [!i!] EXIT
+  CD ..
+  ECHO.
+  ECHO select Device.....
 
-REM -----------------------------------------
-REM choose device
-REM -----------------------------------------
+  REM -----------------------------------------
+  REM choose device
+  REM -----------------------------------------
 
-SET /P dirCounter=
-CLS
-ECHO archive Device: !deviceDir[%dirCounter%]!
-ECHO -------------------------------------------
-ECHO.
+  SET /P dirCounter=
+  IF %dirCounter% == !i! GOTO end
 
-REM -----------------------------------------
-REM clean archive
-REM -----------------------------------------
-ECHO clean archive files !deviceDir[%dirCounter%]!....
-DEL _customDevices\!deviceDir[%dirCounter%]!\data\customDevice\*.json
-DEL _customDevices\!deviceDir[%dirCounter%]!\firmware\*.bin
-DEL _customDevices\!deviceDir[%dirCounter%]!\firmware\*.tar
-DEL _customDevices\!deviceDir[%dirCounter%]!\src\customDevice\customDevice*.*
-ECHO done
-ECHO.
+  CLS
+  ECHO archive Device: !deviceDir[%dirCounter%]!
+  ECHO -------------------------------------------
+  ECHO.
 
-REM -----------------------------------------
-REM copy Files
-REM -----------------------------------------
+  REM -----------------------------------------
+  REM clean archive
+  REM -----------------------------------------
+  ECHO clean archive files !deviceDir[%dirCounter%]!....
+  DEL _customDevices\!deviceDir[%dirCounter%]!\data\customDevice\*.json
+  DEL _customDevices\!deviceDir[%dirCounter%]!\firmware\*.bin
+  DEL _customDevices\!deviceDir[%dirCounter%]!\firmware\*.tar
+  DEL _customDevices\!deviceDir[%dirCounter%]!\src\customDevice\customDevice*.*
+  ECHO done
+  ECHO.
 
-ECHO archive files to !deviceDir[%dirCounter%]!
-copy %~d0%~p0data\customDevice\*.json _customDevices\!deviceDir[%dirCounter%]!\data\customDevice
-copy %~d0%~p0update\*.* _customDevices\!deviceDir[%dirCounter%]!\firmware
-copy %~d0%~p0src\customDevice\customDevice*.* _customDevices\!deviceDir[%dirCounter%]!\src\customDevice
-ECHO done
+  REM -----------------------------------------
+  REM copy Files
+  REM -----------------------------------------
 
-GOTO end
+  ECHO archive files to !deviceDir[%dirCounter%]!
+  copy %~d0%~p0data\customDevice\*.json _customDevices\!deviceDir[%dirCounter%]!\data\customDevice
+  copy %~d0%~p0update\*.* _customDevices\!deviceDir[%dirCounter%]!\firmware
+  copy %~d0%~p0src\customDevice\customDevice*.* _customDevices\!deviceDir[%dirCounter%]!\src\customDevice
+  ECHO done
+  ECHO.
+  GOTO end
 
 REM ----------------------------------------------------------------------------
+REM copy new clean OmniESP.json
+REM ----------------------------------------------------------------------------
+
 :copyOmniesp
-CLS
-ECHO copy OmniESP to data DIR
-ECHO -------------------------------------------
-ECHO.
-ECHO copy files...
-copy %~d0%~p0_customDevices\OmniESP.json data
+  CLS
+  ECHO copy OmniESP to data DIR
+  ECHO -------------------------------------------
+  ECHO.
+  ECHO copy files...
+  copy %~d0%~p0_customDevices\OmniESP.json data
+  ECHO done
+  ECHO.
+
+REM ----------------------------------------------------------------------------
+REM create NEW Device
+REM ----------------------------------------------------------------------------
+
+:newDevice
+  CLS
+  ECHO create NEW Device
+  ECHO -------------------------------------------
+  ECHO.
+  ECHO in process...
+  ECHO.
+  GOTO end
+
+REM ----------------------------------------------------------------------------
+REM create NEW Module
+REM ----------------------------------------------------------------------------
+
+:newModule
+  CLS
+  ECHO create NEW Module
+  ECHO -------------------------------------------
+  ECHO.
+  ECHO in process...
+  ECHO.
+  GOTO end
 
 :end
