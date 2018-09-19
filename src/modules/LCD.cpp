466306cf7aaa2596b2ffb5317803b60286c2c42e
Wobbler <FlorianSadzeck@googlemail.com>
2018-09-19 09:57:07 +0200

    PullRequest for LCD

--- src/modules/LCD.cpp
+++ src/modules/LCD.cpp
@@ -6,8 +6,8 @@
 //===============================================================================
 LCD::LCD(string name, LOGGING &logging, TopicQueue &topicQueue, int sda, int scl)
         :Module(name, logging, topicQueue),
-         sda(sda), scl(scl),
-         ssd1306(SSD1306_I2C_adr, sda, scl)
+         sda(sda), scl(scl)//,
+         //ssd1306(SSD1306_I2C_adr, sda, scl)
          {}
 
 
@@ -20,6 +20,28 @@ LCD::LCD(string name, LOGGING &logging, TopicQueue &topicQueue, int sda, int scl
 void LCD::start() {
   Module::start();
 
+  ssd1306_setFixedFont(ssd1306xled_font6x8);
+  ssd1306_128x64_i2c_init();
+  ssd1306_fillScreen( 0x00 );
+  ssd1306_clearScreen();
+
+/*
+  ssd1306_setFixedFont(ssd1306xled_font6x8);
+  ssd1306_clearScreen();
+  ssd1306_printFixed(0,  8, "Normal text", STYLE_NORMAL);
+  ssd1306_printFixed(0, 16, "Bold text", STYLE_BOLD);
+  ssd1306_printFixed(0, 24, "Italic text", STYLE_ITALIC);
+  ssd1306_negativeMode();
+  ssd1306_printFixed(0, 32, "Inverted bold", STYLE_BOLD);
+  ssd1306_positiveMode();
+  delay(3000);
+  ssd1306_clearScreen();
+  ssd1306_printFixedN(0, 0, "N3", STYLE_NORMAL, FONT_SIZE_8X);
+  delay(3000);
+*/
+
+
+/*
   logging.info("starting LCD SSD1306");
   Wire.begin(sda, scl);
   ssd1306.init();
@@ -27,9 +49,10 @@ void LCD::start() {
   ssd1306.setContrast(255);
   ssd1306.clear();
 
-  //println("Hello World!", ArialMT_Plain_24, 0);
-  //println("Hello World!", ArialMT_Plain_16, 24);
-  //println("Hello World!", ArialMT_Plain_10, 49);
+  ssd1306.println("Hello World!", ArialMT_Plain_24, 0);
+  ssd1306.println("Hello World!", ArialMT_Plain_16, 24);
+  ssd1306.println("Hello World!", ArialMT_Plain_10, 49);
+*/
 }
 
 //...............................................................................
@@ -52,10 +75,20 @@ String LCD::getVersion() {
 //  printline
 //...............................................................................
 void LCD::println(String txt, int yPos) {
-  ssd1306.setTextAlignment(TEXT_ALIGN_LEFT);
-  ssd1306.setFont(ArialMT_Plain_16);
-  ssd1306.drawString(0, yPos, txt);
-  ssd1306.display();
+
+
+
+
+
+  ssd1306_printFixed(0,  yPos, txt.c_str(), STYLE_NORMAL);
+
+
+
+
+  //ssd1306.setTextAlignment(TEXT_ALIGN_LEFT);
+  //ssd1306.setFont(ArialMT_Plain_16);
+  //ssd1306.drawString(0, yPos, txt);
+  //ssd1306.display();
 
   //println("Hello World!", ArialMT_Plain_24, 0);
   //println("Hello World!", ArialMT_Plain_16, 24);
