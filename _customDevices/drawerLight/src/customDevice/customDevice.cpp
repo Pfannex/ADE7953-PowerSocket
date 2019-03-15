73a2a8ddc716c6765f90d6ebe0d149a9d5e0376c
Pf@nne <pf@nne-mail.de>
2019-03-15 16:27:54 +0100

    Device Drawerlight V1.00 final

--- _customDevices/drawerLight/src/customDevice/customDevice.cpp
+++ _customDevices/drawerLight/src/customDevice/customDevice.cpp
@@ -9,8 +9,7 @@
 //  Device public
 //-------------------------------------------------------------------------------
 customDevice::customDevice(LOGGING &logging, TopicQueue &topicQueue, FFS &ffs)
-    : Device(logging, topicQueue, ffs){//,
-    //WS_DI("WS2812", logging, topicQueue, WS_PIN, LEDCOUNT) {
+    : Device(logging, topicQueue, ffs){
 
   type= String(DEVICETYPE);
   version = String(DEVICEVERSION);
@@ -22,15 +21,6 @@ customDevice::customDevice(LOGGING &logging, TopicQueue &topicQueue, FFS &ffs)
 void customDevice::start() {
 
   Device::start();
-  /*
-  logging.info("starting " +
-               button.getVersion()); // only first time a class is started
-  */
-
-  //WS_DI.start();
-  //String col = ffs.deviceCFG.readItem("drawer1_COLOR");
-  //WS_DI.color = (int) strtol( &col[1], NULL, 16);
-
 
   pinMode(QRE_PIN, OUTPUT);
   pinMode(S0, OUTPUT);
@@ -49,13 +39,6 @@ void customDevice::start() {
     setStrip(0);
   }
 
-
-
-  //ws.begin();
-  //ws.clear();
-
-
-
   logging.info("device running");
 }
 
@@ -73,21 +56,29 @@ void customDevice::handle() {
 String customDevice::set(Topic &topic) {
   /*
   ~/set
-  └─device             (level 2)
-    └─drawer1          (level 3)
+  └─device               (level 2)
+    └─drawer             (level 3)
+      └─color            (level 4)
+      └─index            (level 4) individual setting
+      └─name             (level 4) individual setting
+      └─individualColor  (level 4) individual setting
+      └─usecase          (level 4) individual setting
+
+    └─floor            (level 3)
       └─color          (level 4)
       └─state          (level 4)
-    └─drawer2          (level 3)
-      └─color          (level 4)
+    └─drawerx          (level 3)  x=[1-16] drawer & floor
       └─state          (level 4)
+      └─color          (level 4)
   */
 
   logging.debug("device set topic " + topic.topic_asString() + " to " +
                 topic.arg_asString());
   String ret = TOPIC_NO;
 
-//store all drawer/color
+  //DRAWER---------------------------
   if (topic.itemIs(3, "drawer")) {
+    //store all drawer/color
     if (topic.itemIs(4, "color")) {
       String str = topic.getArg(0);
       if (!str.startsWith("#")) str = "#" + str;
@@ -101,10 +92,59 @@ String customDevice::set(Topic &topic) {
       ffs.deviceCFG.saveFile();
       return TOPIC_OK;
     }
+    //individual settings
+    //index-------------------
+    if (topic.itemIs(4, "index")) {
+      String strIndex = topic.getArg(0);
+      if (strIndex == "next"){
+        (index < CHANNELSCOUNT) ? (index++) : (index = 1);
+        topicQueue.put("~/event/device/drawer/index " + String(index));
+      }else if (strIndex == "previous"){
+        (index > 1) ? (index--) : (index = CHANNELSCOUNT);
+        topicQueue.put("~/event/device/drawer/index " + String(index));
+      }else{
+        index = topic.getArgAsLong(0);
+      }
+
+      String strItem = "drawer" + String(index);
+      name = ffs.deviceCFG.readItem(strItem + "_NAME");
+      color = ffs.deviceCFG.readItem(strItem + "_COLOR");
+      usecase = ffs.deviceCFG.readItem(strItem + "_USECASE");
+      topicQueue.put("~/event/device/drawer/name " + name);
+      topicQueue.put("~/event/device/drawer/individualColor " + color);
+      String uc = (usecase == "drawer") ? ("0") : ("1");
+      topicQueue.put("~/event/device/drawer/usecase " + uc);
+      return TOPIC_OK;
+    }
+    //name-------------------
+    if (topic.itemIs(4, "name")) {
+      String str = topic.getArg(0);
+      String strItem = "drawer" + String(index);
+      ffs.deviceCFG.writeItem(strItem + "_NAME", str);
+      ffs.deviceCFG.saveFile();
+      return TOPIC_OK;
+    }
+    //individualColor-------------------
+    if (topic.itemIs(4, "individualColor")) {
+      String str = topic.getArg(0);
+      String strItem = "drawer" + String(index);
+      ffs.deviceCFG.writeItem(strItem + "_COLOR", str);
+      ffs.deviceCFG.saveFile();
+      return TOPIC_OK;
+    }
+    //usecase-------------------
+    if (topic.itemIs(4, "usecase")) {
+      String str = topic.getArg(0);
+      String strItem = "drawer" + String(index);
+      ffs.deviceCFG.writeItem(strItem + "_USECASE", str);
+      ffs.deviceCFG.saveFile();
+      return TOPIC_OK;
+    }
   }
 
+  //FLOOR---------------------------
   if (topic.itemIs(3, "floor")) {
-//store all floor/color
+    //store all floor/color-------------------
     if (topic.itemIs(4, "color")) {
       String str = topic.getArg(0);
       if (!str.startsWith("#")) str = "#" + str;
@@ -118,7 +158,7 @@ String customDevice::set(Topic &topic) {
       ffs.deviceCFG.saveFile();
       return TOPIC_OK;
     }
-//switch all floor
+    //switch all floor-------------------
     if (topic.itemIs(4, "state")) {
       for (size_t i = 0; i < CHANNELSCOUNT; i++) {
         setChannel(i);
@@ -129,17 +169,17 @@ String customDevice::set(Topic &topic) {
           }else{
             setStrip(0);
           }
-          //setStrip(i+1, topic.getArgAsLong(0));
         }
       }
       return TOPIC_OK;
     }
   }
 
+  //DRAWER[i]----------------------------------
   for (size_t i = 0; i < CHANNELSCOUNT; i++) {
     char ch[80] = "drawer";
     strcat (ch, String(i+1).c_str());
-//switch individual drawer/floorlight
+    //switch individual drawer/floorlight------
     if (topic.itemIs(3, ch)){
       if (topic.itemIs(4, "state")){
         setChannel(i);
@@ -150,7 +190,7 @@ String customDevice::set(Topic &topic) {
         }
         return TOPIC_OK;
       }
-//set individual drawer/floorlight color
+      //set individual drawer/floorlight color
       if (topic.itemIs(4, "color")){
         setChannel(i);
         String str = topic.getArg(0);
@@ -162,87 +202,6 @@ String customDevice::set(Topic &topic) {
     }
   }
   return ret;
-
-
-
-
-/*
-
-  if (topic.itemIs(3, "drawer1")){
-    if (topic.itemIs(4, "state")) {
-      setChannel(0);
-      if (topic.getArgAsLong(0)){
-        Serial.println("on");
-        String col = ffs.deviceCFG.readItem("drawer1_COLOR");
-        //color = (int) strtol( &col[1], NULL, 16);
-        setStrip(col);
-      }else{
-        Serial.println("off");
-        setStrip(0);
-      }
-      return TOPIC_OK;
-    }else if (topic.itemIs(4, "color")) {
-      String str = topic.getArg(0);
-      //color = (int) strtol( &str[1], NULL, 16);
-      ffs.deviceCFG.writeItem("drawer1_COLOR", str);
-      ffs.deviceCFG.saveFile();
-      setChannel(1);
-      setStrip(str);
-      return TOPIC_OK;
-    }else{
-      return TOPIC_NO;
-    }
-
-  }else if (topic.itemIs(3, "drawer2")){
-    return TOPIC_NO;
-
-  }else{
-    return TOPIC_NO;
-  }
-*/
-
-/*
-
-  if (topic.itemIs(3, "color1")) {
-    String str = topic.getArg(0);
-    d1_OUT.color = (int) strtol( &str[1], NULL, 16);
-    ffs.deviceCFG.writeItem("Drawer1_COLOR", str);
-    ffs.deviceCFG.saveFile();
-    d1_OUT.WS2812_on(1);
-    return TOPIC_OK;
-  } else if (topic.itemIs(3, "power1")) {
-    d1_OUT.WS2812_on(topic.getArgAsLong(0));
-    //setPowerMode(power ? 0 : 1);
-    return TOPIC_OK;
-
-  } else if (topic.itemIs(3, "color2")) {
-    String str = topic.getArg(0);
-    d2_OUT.color = (int) strtol( &str[1], NULL, 16);
-    //ffs.deviceCFG.writeItem("WS2812_02_COLOR", String(d2_OUT.color));
-    ffs.deviceCFG.writeItem("WS2812_02_COLOR", str);
-    ffs.deviceCFG.saveFile();
-    d2_OUT.WS2812_on(1);
-    return TOPIC_OK;
-  } else if (topic.itemIs(3, "power02")) {
-    d2_OUT.WS2812_on(topic.getArgAsLong(0));
-    //setPowerMode(power ? 0 : 1);
-    return TOPIC_OK;
-
-
-
-
-  } else if (topic.itemIs(3, "toggle")) {
-    //setPowerMode(power ? 0 : 1);
-    return TOPIC_OK;
-  } else if (topic.itemIs(3, "led")) {
-    if (topic.itemIs(4, "blink")) {
-      //setLedMode(topic.getArgAsLong(0));
-      return TOPIC_OK;
-    }
-    return TOPIC_OK;
-  } else {
-    return TOPIC_NO;
-  }*/
 }
 
 //...............................................................................
@@ -278,11 +237,11 @@ void customDevice::on_events(Topic &topic){
 //  on request, fillDashboard with values
 //...............................................................................
 String customDevice::fillDashboard() {
-  //topicQueue.put("~/event/device/drawer1/state", QRE_IN.state());
-
-
   topicQueue.put("~/event/device/drawer/color " + ffs.deviceCFG.readItem("drawer_COLOR"));
   topicQueue.put("~/event/device/floor/color " + ffs.deviceCFG.readItem("floor_COLOR"));
+
+  topicQueue.put("~/event/device/drawer/index 1");
+  topicQueue.put("~/set/device/drawer/index 1");
 
   logging.debug("dashboard filled with values");
   return TOPIC_OK;
@@ -299,7 +258,6 @@ void customDevice::setChannel(int channel){
   ((channel & 2) == 2) ? (digitalWrite(S1, HIGH)) : (digitalWrite(S1, LOW));
   ((channel & 4) == 4) ? (digitalWrite(S2, HIGH)) : (digitalWrite(S2, LOW));
   ((channel & 8) == 8) ? (digitalWrite(S3, HIGH)) : (digitalWrite(S3, LOW));
-  //delay(100);
 }
 
 //...............................................................................
@@ -333,30 +291,15 @@ void customDevice::setStrip(int channel, int state){
 //  handle Channels
 //...............................................................................
 void customDevice::handleChannels(){
-/*
-  setStrip(0, 0x00FF00);
-  setStrip(1, 0x00FF00);
-  setStrip(2, 0x00FF00);
-  delay(500);
-
-  setStrip(0, 0xFF0000);
-  setStrip(1, 0x0000FF);
-  setStrip(2, 0xFF00FF);
-  delay(500);
-*/
-
 
   for (byte i = 0; i < CHANNELSCOUNT; i++) {
     //select channel
     setChannel(i);
-    //Serial.println("Channel: " + String(i));
 
     //select settings from ffs
     String strItem = "drawer" + String(i+1);
-    //String col = ffs.deviceCFG.readItem(strItem + "_COLOR");
     String name = ffs.deviceCFG.readItem(strItem + "_NAME");
     int isDrawer = (ffs.deviceCFG.readItem(strItem + "_USECASE") == "drawer") ? (1) : (0);
-    //color = (int) strtol( &col[1], NULL, 16);
 
     if (isDrawer){
       //read QRE-value
@@ -371,7 +314,6 @@ void customDevice::handleChannels(){
       while (digitalRead(QRE_PIN) == HIGH and diff < TIMEOUT){
         diff = micros() - time;
       }
-      //Serial.println("Channel " + String(i) + " = " + String(diff));
 
       //check QRE-value
       unsigned long now = millis();
@@ -380,8 +322,6 @@ void customDevice::handleChannels(){
       if (now - lastChangeTime[i] > DEBOUNCETIME){
         lastChangeTime[i] = now;
 
-        //String IO = String(i);
-        //Serial.println(QRE_Value);
         String eventPrefix= "~/event/device/drawer/" + name + "/";
 
         if (diff > THRESHOLD_ON && !pinState[i]){
@@ -398,59 +338,4 @@ void customDevice::handleChannels(){
       }
     }
   }
-
-
-/*
-  for (byte i = 0; i < CHANNELSCOUNT; i++) {
-    //select channel
-    setChannel(i);
-    //Serial.println("Channel: " + String(i));
-
-    //select color from ffs
-    if (i == 0) WS_DI.color = 0x0000FF;
-    if (i == 1) WS_DI.color = 0x00FF00;
-    if (i == 2) WS_DI.color = 0xFF0000;
-    if (i == 0) WS_DI.count = 17;
-    if (i == 1) WS_DI.count = 17;
-    if (i == 2) WS_DI.count = 38;
-
-    //read QRE-value
-    pinMode(QRE_PIN, OUTPUT);
-    digitalWrite(QRE_PIN, HIGH);
-    delayMicroseconds(10);
-    pinMode(QRE_PIN, INPUT );
-
-    long time = micros();
-    //time how long the input is HIGH, but quit after 3ms as nothing happens after that
-    int diff = 0;
-    while (digitalRead(QRE_PIN) == HIGH and diff < TIMEOUT){
-      diff = micros() - time;
-    }
-    //Serial.println("Channel " + String(i) + " = " + String(diff));
-
-    //check QRE-value
-    unsigned long now = millis();
-
-    //debouncing and state detection
-    if (now - lastChangeTime[i] > DEBOUNCETIME){
-      lastChangeTime[i] = now;
-
-      String IO = String(i);
-      //Serial.println(QRE_Value);
-      String eventPrefix= "~/event/device/drawer" + String(i) + "/";
-
-      if (diff > THRESHOLD_ON && !pinState[i]){
-        pinState[i] = 1;
-        logging.debug("Drawer " + String(i) + " open");
-        topicQueue.put(eventPrefix + "/state 1");
-        WS_DI.WS2812_on(1);
-      }else if (diff < THRESHOLD_OFF && pinState[i]){
-        pinState[i] = 0;
-        logging.debug("Drawer " + String(i) + " closed");
-        topicQueue.put(eventPrefix + "/state 0");
-        WS_DI.WS2812_on(0);
-      }
-    }
-  }
-  */
 }
--- _customDevices/drawerLight/src/customDevice/customDevice.h
+++ _customDevices/drawerLight/src/customDevice/customDevice.h
@@ -9,8 +9,6 @@
 
 // modules required by device
 #include "modules/GPIO.h"
-//#include "modules/WS2812.h"
-//#include <Adafruit_NeoPixel.h>
 #include <FastLED.h>
 
 //###############################################################################
@@ -18,7 +16,7 @@
 //###############################################################################
 
 #define DEVICETYPE      "drawerLight"
-#define DEVICEVERSION   "v1"
+#define DEVICEVERSION   "V1"
 
 class customDevice : public Device {
 
@@ -32,84 +30,20 @@ public:
   void on_events(Topic &topic);
 
 private:
-  //WS2812  WS_DI;
-  //Adafruit_NeoPixel ws = Adafruit_NeoPixel(LEDCOUNT, WS_PIN, NEO_RGB + NEO_KHZ400);
-
   CRGB leds[LEDCOUNT];
+  void setChannel(int channel);
   void setStrip(int color);
   void setStrip(String col);
   void setStrip(int channel, int state);
-
-  int color = 0;
-  int count = 0;
-
-  void setChannel(int channel);
   void handleChannels();
-  int pinState[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};   // the last pin state
-  unsigned long lastChangeTime[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};   // last time pin changed
+
+  int index = 1;
+  String name = "";
+  String color = "";
+  String usecase = "";
+
+  //int pinState[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};   // the last pin state
+  //unsigned long lastChangeTime[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};   // last time pin changed
+  int pinState[16] = {};   // the last pin state
+  unsigned long lastChangeTime[16] = {};   // last time pin changed
 };
-
-/*
-# ---------------------------------------------------
-# DRAWER
-# FHEM:
-# ---------------------------------------------------
-define drawer MQTT_DEVICE
-attr drawer IODev MQTT_Broker
-attr drawer publishSet_d1color Drawer_01/set/device/drawer1/color
-attr drawer publishSet_d2color Drawer_01/set/device/drawer2/color
-attr drawer publishSet_color Drawer_01/set/device/drawer3/color
-attr drawer publishSet_off Drawer_01/set/device/drawer3/state
-attr drawer publishSet_on Drawer_01/set/device/drawer3/state
-attr drawer room 02_Küche
-attr drawer stateFormat state
-attr drawer subscribeReading_drawer1 Drawer_01/event/device/drawer1/state
-attr drawer subscribeReading_drawer2 Drawer_01/event/device/drawer2/state
-attr drawer subscribeReading_drawer3 Drawer_01/event/device/drawer3/state
-attr drawer verbose 5
-
-
-#attr drawer publishSet_color         Drawer_01/set/device/drawer3/color
-#attr drawer publishSet_drawer3       Drawer_01/set/device/drawer3/state
-#attr drawer publishSet_fillDashboard Drawer_01/set/device/fillDashboard x
-
-#attr drawer webCmd hue:rgb:rgb ff0000:rgb 98FF23:rgb 0000ff:on:off
-#attr drawer widgetOverride rgb:colorpicker,RGB
-
-#attr drawer publishSet_rgb Drawer_01/set/device/drawer3/color
-
-#---------------------------------------------------
-
-define DrawerColor dummy
-attr DrawerColor alias Drawer
-attr DrawerColor room 02_Küche
-attr DrawerColor setList rgb
-attr DrawerColor verbose 5
-attr DrawerColor webCmd rgb:rgb ff0000:rgb 00ff00:rgb 0000ff:on:off
-attr DrawerColor widgetOverride rgb:colorpicker,RGB
-
-define DrawerColor_n notify DrawerColor:rgb.* set drawer d1color $EVTPART1;;set drawer d2color $EVTPART1
-
-#---------------------------------------------------
-
-define Drawer1.3 dummy
-attr Drawer1.3 alias Floor
-attr Drawer1.3 room 02_Küche
-attr Drawer1.3 setList rgb
-attr Drawer1.3 verbose 5
-attr Drawer1.3 webCmd rgb:rgb ff0000:rgb 00ff00:rgb 0000ff:on:off
-attr Drawer1.3 widgetOverride rgb:colorpicker,RGB
-
-define Drawer1.3n_on notify Drawer1.3:on set drawer on 1
-define Drawer1.3n_off notify Drawer1.3:off set drawer off 0
-define Drawer1.3n_color notify Drawer1.3:rgb.* set drawer color $EVTPART1;;set drawer on 1
-
-define Drawer1.3n_xx notify WZ_INNEN:on set drawer on 1
-define Drawer1.3n_yy notify WZ_INNEN:off set drawer off 0
-
-
-#---------------------------------------------------
-define colorInit notify global:INITIALIZED {use Color}
-attr colorInit room 02_Küche
-
-*/
