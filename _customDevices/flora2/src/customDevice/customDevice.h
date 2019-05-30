7fd65c3dd85de99de35cb40ac7f889d55fb4db16
Dr. Boris Neubert <omega@online.de>
2019-05-30 20:31:43 +0200

    tank and moisture configuration added

--- _customDevices/flora2/src/customDevice/customDevice.h
+++ _customDevices/flora2/src/customDevice/customDevice.h
@@ -8,12 +8,12 @@
 #include "customDeviceSetup.h"
 
 // modules required by device
-#include "modules/GPIO.h"
-#include "Adafruit_BMP280.h"
 #include "AS_BH1750.h"
-#include "Adafruit_VEML6070.h"
-#include "Adafruit_MCP23017.h"
 #include "Adafruit_ADS1015.h"
+#include "Adafruit_BMP280.h"
+#include "Adafruit_MCP23017.h"
+#include "Adafruit_VEML6070.h"
+#include "modules/GPIO.h"
 #include <Wire.h>
 
 //###############################################################################
@@ -38,14 +38,28 @@ public:
   String fillDashboard();
 
 private:
+
+  void inform();
+  void logPollInterval();
+
+  // sensors
   Adafruit_BMP280 pressureSensor;
   AS_BH1750 lightSensor;
   Adafruit_VEML6070 uvSensor;
   Adafruit_MCP23017 mcp;
   Adafruit_ADS1115 ads;
 
-  unsigned long pollInterval= 0;
+  bool pressureSensorIsPresent = false;
+  bool lightSensorIsPresent = false;
+  bool uvSensorIsPresent = false;
+  bool mcpIsPresent = false;
+  bool adsIsPresent = false;
+
+  // polling
+  unsigned long pollInterval = 0;
   unsigned long lastPoll = 0;
+
+  // measurement
   float measureIlluminanceLux();
   float measureTemperatureCelsius();
   float measurePressurehPa();
@@ -55,24 +69,21 @@ private:
   float UVreadingToUVmuWpercm2(uint16_t);
   float voltageToVolumeLiter(float);
   float voltageToMoisturePercent(float);
-
   float measureVoltage(int channel);
-  void switchRelay(int relay, int state);
-  void inform();
-  void logPollInterval();
-  bool pressureSensorIsPresent= false;
-  bool lightSensorIsPresent= false;
-  bool uvSensorIsPresent= false;
-  bool mcpIsPresent= false;
-  bool adsIsPresent= false;
+  void measure();
 
-  void getConfig();
-  void setConfig();
-  void calcConfig();
-  // config tank volume
-  float tVoltage1, tVoltage2, tVolume1, tVolume2, tSlope;
-  // config moisture sensor
-  float mVoltage1, mVoltage2, mMoisture1, mMoisture2, mSlope;
+  float temperature;
+  float pressure;
+  float illuminance;
+  int uv;
+  float uvIntensity;
+  uint16_t uvRiskLevel;
+  float voltage[4];
+  float moisture;
+  float volume;
+
+  // relays
+  void switchRelay(int relay, int state);
 
   GPIOinput button;
   int configMode = 0;
@@ -80,6 +91,24 @@ private:
   GPIOoutput led;
   void setLedMode(int value);
 
+  // config
   void setConfigMode(int value);
+
+  // calibration
+  int configPoint = 0;
+  float configValue= 0.0;
+  void changeConfigPoint(int newConfigPoint);
+  void configApply();
+  float getConfigVoltage(int configPoint);
+  float getConfigValue(int configPoint);
+  void setConfigValue(int configPoint, float value);
+  void informConfig();
+  void getConfig();
+  void setConfig();
+  void calcConfig();
+  // config tank volume
+  float tVoltage1, tVoltage2, tVolume1, tVolume2, tSlope;
+  // config moisture sensor
+  float mVoltage1, mVoltage2, mMoisture1, mMoisture2, mSlope;
 
 };
